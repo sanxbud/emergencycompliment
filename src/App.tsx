89521af4e113019966d85dafdb4fba9d15efb9e0
rook37 <33361387+rook37@users.noreply.github.com>
2025-07-20 23:21:13 -0500

    Adding fun animation to textbox

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,10 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-amber-100">
-        <div className="bg-white shadow-xl p-10 flex items-center justify-center  text-center max-w-2xl rounded-3xl min-h-60 ">
+        <div key={compliment} className="
+          bg-white shadow-xl p-10 flex items-center justify-center  text-center max-w-2xl rounded-3xl min-h-60  
+          animate-[bounce-up_0.4s_ease-out]" 
+          >
           <h1 className="text-[clamp(2rem,3vw,2.33rem)]">{compliment}</h1></div>
         <button className="bg-indigo-200 p-6 m-4 shadow-md rounded-xl hover:bg-indigo-300 hover:scale-105 transform transition duration-200 ease-in-out cursor-pointer" onClick={generateCompliment}><p className="text-xl font-bold">{buttonText}</p></button>
         <div className=""><button className="cursor-pointer m-2 rounded-xl hover:scale-115 transform transition duration-200 ease-in-out" onClick={submitCompliment}><p className="font-bold text-sm text-indigo-950">Suggest a compliment!</p></button></div>
