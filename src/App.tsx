005737cd60f2e0dbd847ab710385c0c10f02031c
rook37 <33361387+rook37@users.noreply.github.com>
2025-07-20 22:16:44 -0500

    Adding dummy suggestion box

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,16 @@ function App() {
         setCompliment(compliments[getRand()])
     }
 
+    const submitCompliment =()=>{
+    let x =2;
+  }
+
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-indigo-950">
         <div className="bg-white shadow-md p-10 text-center max-w-2xl rounded-xl "><h1>{compliment}</h1></div>
         <div className="bg-amber-300 p-6 m-4 rounded-xl"><button  onClick={generateCompliment}><p className="text-xl font-bold">Generate New Compliment</p></button></div>
+        <div className="p-4 m-4"><button onClick={submitCompliment}><p>Submit a compliment!</p></button></div>
       </div>    
       <footer className="text-center text-white bg-indigo-950 text-sm py-4">made with ♡ by sanjay budhia</footer>
     </>
