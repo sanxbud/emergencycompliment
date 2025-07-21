386eb65e7a25e8a269b14a58a9ad7a287b79c20d
rook37 <33361387+rook37@users.noreply.github.com>
2025-07-20 23:20:15 -0500

    Adding animation to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import { compliments, generateText } from './compliments'
 
 
 function App() {
-  const getRand = (array) => { 
-    return array[Math.floor(Math.random()*(array.length-1))]    
+  const getRand = (array:string[]) => { 
+    return array[Math.floor(Math.random()*(array.length))]    
   }
 
 //  const rand = getRand()  
-  const [compliment, setCompliment] = useState(Math.floor(Math.random()*(compliments.length-1)))
+  const [compliment, setCompliment] = useState(getRand(compliments))
   const [buttonText, setButtonText] = useState(generateText[0])
     const generateCompliment = () => {
       // rand =  Math.floor(Math.random()*(compliments.length-1))
@@ -26,9 +26,11 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-amber-100">
-        <div className="bg-white shadow-xl p-10 flex items-center justify-center  text-center max-w-2xl rounded-3xl min-h-60 "><h1 className="text-[clamp(2rem,3vw,2.33rem)]">{compliment}</h1></div>
-        <button className="bg-indigo-200 p-6 m-4 shadow-md rounded-xl" onClick={generateCompliment}><p className="text-xl font-bold">{buttonText}</p></button>
-        <div className="m-2"><button onClick={submitCompliment}><p className="font-bold text-fuschia-700">Suggest a compliment!</p></button></div>
+        <div className="bg-white shadow-xl p-10 flex items-center justify-center  text-center max-w-2xl rounded-3xl min-h-60 ">
+          <h1 className="text-[clamp(2rem,3vw,2.33rem)]">{compliment}</h1></div>
+        <button className="bg-indigo-200 p-6 m-4 shadow-md rounded-xl hover:bg-indigo-300 hover:scale-105 transform transition duration-200 ease-in-out cursor-pointer" onClick={generateCompliment}><p className="text-xl font-bold">{buttonText}</p></button>
+        <div className=""><button className="cursor-pointer m-2 rounded-xl hover:scale-115 transform transition duration-200 ease-in-out" onClick={submitCompliment}><p className="font-bold text-sm text-indigo-950">Suggest a compliment!</p></button></div>
+
       </div>    
       <footer className="text-center text-amber-100 bg-indigo-950 font-bold text-sm py-4">made with ♡ by sanjay budhia</footer>
     </>
